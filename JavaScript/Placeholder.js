38282bf8332937896c9ccf099715c8d8954c85ba
federico.p98@outlook.it <53578288+pheasant98@users.noreply.github.com>
2020-01-01 12:07:48 +0100

    Finito CSS Home + creazione regole standard CSS

--- JavaScript/Placeholder.js
+++ JavaScript/Placeholder.js
@@ -2,16 +2,21 @@ window.onload = menu;
 function menu()
 {
     document.getElementById("HamburgerMenu").addEventListener("click",touch);//ascoltatore
+    document.getElementById("UserTools").addEventListener("click", touch2);
 
     function touch()//classe gestione ascoltatore
     {
-
         var Hmenu=document.getElementById("menu");//prende l'id dell'elemento cliccato
         Hmenu.classList.toggle("show"); //inserisci dentro al tag con l'id cliccato, la nuova classe
         var Content=document.getElementById("content");
         Content.classList.toggle("hide");
         var Breadcrumbs=document.getElementById("breadcrumbs");
         Breadcrumbs.classList.toggle("hide");
+    }
+
+    function touch2() {
+        var UserM=document.getElementById("UserMenu");
+        UserM.classList.toggle("show");
     }
 
 }
