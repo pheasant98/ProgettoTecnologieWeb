77f3aac48a9fc2176476fda2d3a5b4fcca0a2ca2
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-24 12:22:36 +0200

    Fix vari

--- PHP/Utilities.php
+++ PHP/Utilities.php
@@ -10,28 +10,26 @@
         return date("Y-m-d", $timestamp);
     }
 
-    function getAuthenticationMenu($is_authenticated, $is_index = false) {
-        $path = "";
-        if ($is_index) {
-            $path = "PHP/";
-        }
-
+    function getAuthenticationMenu($is_authenticated) {
         if ($is_authenticated) {
-            $login = " . $path . "AreaPersonale.php\" title=\"Area utente\" role=\"button\" aria-label=\"vai alla pagina del profilo utente\" tabindex=\"6\">
-                                    Area utente
+            $login = "<div id=\"LoginMenu\">
+                        <ul>
+                            <li>
+                                <a id=\"areaPersonale\" class=\"Button\" href=\"AreaPersonale.php\" title=\"Area personale\" role=\"button\" aria-label=\"Vai alla pagina del profilo utente\" tabindex=\"6\">
+                                    Area personale
                                 </a>
                             </li>
                             <li>
-                                <a id=\"logOut\" class=\"Button\" href=\"" . $path . "Logout.php\" title=\"LogOut\" role=\"button\" aria-label=\"esci dal tuo profilo\" tabindex=\"7\" xml:lang=\"en\">
-                                    LogOut
+                                <a id=\"logout\" class=\"Button\" href=\"Logout.php\" title=\"Logout\" role=\"button\" aria-label=\"Esci dal tuo profilo\" tabindex=\"7\" xml:lang=\"en\">
+                                    Logout
                                 </a>
                             </li>
                         </ul>
                       </div>";
         } else {
             $login = "<div id=\"LoginMenu\">
-                        <a id=\"logIn\" class=\"Button\" href=\"" . $path . "Login.php\" title=\"LogIn\" role=\"button\" aria-label=\"vai alla pagina di login\" tabindex=\"4\" xml:lang=\"en\">
-                            LogIn
+                        <a id=\"login\" class=\"Button\" href=\"Login.php\" title=\"Login\" role=\"button\" aria-label=\"vai alla pagina di login\" tabindex=\"4\" xml:lang=\"en\">
+                            Login
                         </a>
                       </div>";
         }
