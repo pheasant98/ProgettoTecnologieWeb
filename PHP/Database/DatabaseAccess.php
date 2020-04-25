f2431f4124369159886e62876eaea0741beaefdd
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-25 14:53:01 +0200

    Update DatabaseAccess.php

--- PHP/Database/DatabaseAccess.php
+++ PHP/Database/DatabaseAccess.php
@@ -3,7 +3,7 @@
 class DatabaseAccess {
     private $connection;
 
-    public function __construct($database = "mtoffole", $pass = "", $user = "mtoffole", $host = "localhost") {
+    public function __construct($database = "mtoffole", $pass = "yu0juo9GeiPheiqu", $user = "mtoffole", $host = "localhost:8080") {
         if (!($this->connection = @new mysqli($host, $user, $pass, $database))) {
             error_log("Error number: " . $this->connection->connect_errno . " Error message: " . $this->connection->connect_error);
             echo "I dati non sono al momento disponibili. Se l'errore persiste si prega di segnalarlo al supporto tecnico.";
