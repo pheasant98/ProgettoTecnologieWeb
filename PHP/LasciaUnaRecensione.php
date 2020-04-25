1b8092d4a41550cabbbc4c029a41b13e661746a3
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-04-25 15:25:17 +0200

    Update LasciaUnaRecensione.php

--- PHP/LasciaUnaRecensione.php
+++ PHP/LasciaUnaRecensione.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once ("Controller\LoginController.php");
-require_once("Controller\ReviewsController.php");
+require_once ("Controller/LoginController.php");
+require_once ("Controller/ReviewsController.php");
 
 session_start();
 
