35eb513289ec2c03d452cb0d7f686189250cf94e
Max09081998 <massimotoffoletto98@gmail.com>
2020-04-26 12:48:44 +0200

    Fix pagina evento
    Manca ancora il bottone precedente e successivo

--- PHP/Controller/EventsController.php
+++ PHP/Controller/EventsController.php
@@ -3,8 +3,49 @@
 require_once ('Repository/EventsRepository.php');
 
 class EventsController {
-    public static function getEvents() {
-        $events = new EventsRepository();
+    private $events;
 
+    public function __construct() {
+        $this->events = new EventsRepository();
+    }
+
+    public function getEventsCount() {
+        return mysqli_fetch_assoc($this->events->getEventsCount());
+    }
+
+    public function getEvents($offset) {
+        $result_set = $this->events->getEvents($offset);
+        $id_ref = "e";
+        $button_ref = "buttonBack";
+        $counter = 1;
+        $content = "";
+        while($row = mysqli_fetch_assoc($result_set)) {
+            $content .= "
+                <dt id=\"$id_ref . '' . $counter\">
+                     <a href=\"ContenutoSingolo.php?type=evento&id=" . $row["ID"] . "\" aria-label=\"Vai all'evento\">" . $row["Titolo"] . "</a>
+                </dt>
+                <dd>
+                    <a href=\"#" . ($result_set->num_rows == $counter ? $button_ref : $id_ref . ($counter+1)) . "\" class=\"skipInformation\" aria-label=\"Salta l'evento\">Salta l'evento</a>
+    
+                    <p>
+                        Data inizio evento: " . $row["DataInizio"] . "
+                    </p>
+                    
+                    <p>
+                        Data chiusura evento: " . $row["DataFine"] . "
+                    </p>
+
+                    <p>
+                        Tipologia: " . $row["Tipologia"] . "
+                    </p>
+
+                    <p>
+                        " . $row["Descrizione"] . "
+                    </p>
+                </dd>
+            ";
+            $counter++;
+        }
+        return $content;
     }
 }
