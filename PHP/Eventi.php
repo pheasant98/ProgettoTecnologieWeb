e4a816754189b012bcd67b6d208b0a7ea1a5fcac
Max09081998 <massimotoffoletto98@gmail.com>
2020-04-27 12:14:20 +0200

    Ludo 22 Sistemazione php eventi

--- PHP/Eventi.php
+++ PHP/Eventi.php
@@ -9,17 +9,32 @@ require_once ("Controller/EventsController.php");
 
 session_start();
 
+$events_controller = new EventsController();
+$event_counter = $events_controller->getEventsCount();
+
+if ($_GET["page"] < 1 || (($_GET["page"] - 1) > ($event_counter["Totale"] / 5))) {
+    header('Location: Error.php');
+}
+
 $document = file_get_contents("../HTML/Eventi.html");
 $login = LoginController::getAuthenticationMenu();
 
-$events_controller = new EventsController();
-$event_counter = $events_controller->getEventsCount();
 $offset = ($_GET["page"] - 1) * 5;
 $event_list = "<dl class=\"clickableList\">" . $events_controller->getEvents($offset) . "</dl>";
+$back_events = "";
+if ($_GET["page"] == 1) {
+    $back_events = "Eventi.php?page=" . ($_GET["page"] - 1);
+}
+$next_events = "Eventi.php?page=" . ($_GET["page"] + 1);
+if (($_GET["page"] * 5) >= $event_counter) {
+    $next_events = "";
+}
 
 $document = str_replace("<span id=\"loginMenuPlaceholder\"/>", $login, $document);
 $document = str_replace("<span id=\"eventNumberPlaceholder\"/>", $event_counter["Totale"], $document);
 $document = str_replace("<span id=\"eventListPlaceholder\"/>", $event_list, $document);
+$document = str_replace("<span id='buttonBackPlaceholder'/>", $back_events, $document);
+$document = str_replace("<span id='buttonNextPlaceholder'/>", $next_events, $document);
 
 echo $document;
 
