d0ed30cc4c3776061349032a3bb8e6e91e2ebe0d
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-05-01 17:49:50 +0200

    Non so bene cosa ho fatto ma devo committare

--- PHP/AreaPersonale.php
+++ PHP/AreaPersonale.php
@@ -13,10 +13,48 @@ if (!LoginController::isAuthenticatedUser()) {
     header('Location: Error.php');
 }
 
+$users_controller = new UsersController();
+$user = $users_controller->getUser($_SESSION['Username']);
+
+if ($user['Admin'] === 1) {
+    $operations = '
+        <li>
+            <a id="ModificaAdmin" class="button userOperationButton" href="ModificaDatiUtente.html" title="vai alla pagina modifica dati utente" role="button" aria-label="vai alla pagina modifica dati utente">Modifica profilo</a>
+        </li>
+
+        <li>
+            <a id="Inserisci" class="button userOperationButton" href="Inserisci.html" title="vai alla pagina inserisci pagina" role="button" aria-label="vai alla pagina inserisci pagina">Inserisci pagina</a>
+        </li>
+
+        <li>
+            <a id="GestioneUtenti" class="button userOperationButton" href="GestioneUtenti.html" title="vai alla pagina Gestione Utenti" role="button" aria-label="vai alla pagina iGestione Utenti">Gestione utenti</a>
+        </li>
+
+        <li>
+            <a id="ContenutoSito" class="button userOperationButton" href="GestioneContenuti.html" title="vai alla pagina Contenuto Sito" role="button" aria-label="vai alla pagina Contenuto Sito">Contenuto sito</a>
+        </li>
+    ';
+} else {
+    $operations = '
+        <li>
+            <a href="ModificaDatiUtente.html" id="ModificaUser" class="button userOperationButton" title="Modifica dati profilo" aria-label="Vai alla pagina per la modifica dei dati del profilo">Modifica profilo</a>
+        </li>
+
+        <li>
+            <a href="Recensione.html" class="button userOperationButton" title="Gestisci le tue recensioni" aria-label="Vai alla pagina per la gestione delle recensioni lasciate">Gestisci le recensioni</a>
+        </li>
+
+        <li>
+            <a href="LasciaUnaRecensione.html" class="button userOperationButton" title="Lascia una nuova recensione" aria-label="Vai alla pagina per lasciare una nuova recensione">Lascia una recensione</a>
+        </li>
+    ';
+}
+
 $document = file_get_contents('../HTML/AreaPersonale.html');
 $login = LoginController::getAuthenticationMenu();
 
 $document = str_replace("<span id='loginMenuPlaceholder'/>", $login, $document);
+$document = str_replace("<span id='operationsPlaceholder'/>", $operations, $document);
 
 echo $document;
 
