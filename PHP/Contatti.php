74ffae5b8348e2b8ecf2e5f68ba0121768002f9d
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-05-01 11:12:41 +0200

    Fix ocd perché si

--- PHP/Contatti.php
+++ PHP/Contatti.php
@@ -6,12 +6,11 @@ error_reporting(E_ALL);
 
 session_start();
 
-require_once ("Controller/LoginController.php");
+require_once ('Controller/LoginController.php');
 
 $document = file_get_contents('../HTML/Contatti.html');
 $login = LoginController::getAuthenticationMenu();
-
-$document = str_replace("<span id=\"loginMenuPlaceholder\"/>", $login, $document);
+$document = str_replace("<span id='loginMenuPlaceholder'/>", $login, $document);
 
 echo $document;
 
--- PHP/LasciaUnaRecensione.php
+++ PHP/LasciaUnaRecensione.php
@@ -4,36 +4,36 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once ("Controller/LoginController.php");
-require_once ("Controller/ReviewsController.php");
+require_once ('Controller/LoginController.php');
+require_once ('Controller/ReviewsController.php');
 
 session_start();
 
-# if (!LoginController::isAuthenticatedUser()) {
-#     header('Location: Error.php');
-# }
+if (!LoginController::isAuthenticatedUser()) {
+    header('Location: Error.php');
+}
 
-$message = "";
+$message = '';
 
-if (isset($_POST['submit']) && $_POST['submit'] == 'Invia') {
+if (isset($_POST['submit']) && $_POST['submit'] === 'Invia') {
     $title = $_POST['title'];
     $content = $_POST['content'];
 
     $message = ReviewsController::addReview($title, $content, $_SESSION['Username']);
 }
 
-if ($message == "") {
-    $title = "";
-    $content = "";
+if ($message === '') {
+    $title = '';
+    $content = '';
 }
 
-$document = file_get_contents("../HTML/LasciaUnaRecensione.html");
+$document = file_get_contents('../HTML/LasciaUnaRecensione.html');
 $login = LoginController::getAuthenticationMenu();
 
-$document = str_replace("<span id=\"loginMenuPlaceholder\"/>", $login, $document);
+$document = str_replace("<span id='loginMenuPlaceholder'/>", $login, $document);
+$document = str_replace("<span id='outputMessagePlaceholder'/>", $message, $document);
 $document = str_replace("<span id='titleValuePlaceholder'/>", $title, $document);
 $document = str_replace("<span id='contentValuePlaceholder'/>", $content, $document);
-$document = str_replace("<span id=\"outputMessagePlaceholder\"/>", $message, $document);
 
 echo $document;
 
