7f405a7157a3a22c238c8c2aef5376c5d6652aae
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-05-07 23:16:20 +0200

    Implementazione pagina dell'area personale (non ancora testa in attesa del login)

--- PHP/AreaPersonale.php
+++ PHP/AreaPersonale.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once ('Utilities/DateUtilities.php');
 require_once ('Controller/LoginController.php');
 require_once ('Controller/UsersController.php');
 
@@ -16,9 +17,22 @@ if (!LoginController::isAuthenticatedUser()) {
 $users_controller = new UsersController();
 $user = $users_controller->getUser($_SESSION['Username']);
 
-$operations = '<ul id="operationsList" class="navigation">';
+$user_data = '
+    <dt>Nome:</dt>
+    <dd>' . $user['Nome'] . '</dd>
+    <dt>Cognome:</dt>
+    <dd>' . $user['Cognome'] . '</dd>
+    <dt>Sesso:</dt>
+    <dd>' . ($user['Sesso'] === 'M' ? 'Maschile' : ($user['Sesso'] === 'F' ? 'Femminile' : 'Non specificato')) . '</dd>
+    <dt>Data di nascita:</dt>
+    <dd>' . DateUtilities::englishItalianDate($user['DataNascita']) . '</dd>
+    <dt>Indirizzo <span xml:lang="en">e-mail</span>:</dt>
+    <dd>' . $user['Email'] . '</dd>
+';
+
 if ($user['Admin'] === 1) {
-    $operations .= '    
+    $functionalities = 'Ora che hai effettuato l\'accesso all\'area personale, puoi utilizzare il tuo <span xml:lang="en">account</span> per gestire gli utenti e i contenuti del sito.';
+    $operations = '    
         <li>
             <a id="ModificaAdmin" class="button userOperationButton" href="ModificaDatiUtente.html" title="vai alla pagina modifica dati utente" role="button" aria-label="vai alla pagina modifica dati utente">Modifica profilo</a>
         </li>
@@ -36,6 +50,7 @@ if ($user['Admin'] === 1) {
         </li>
     ';
 } else {
+    $functionalities = 'Ora che hai effettuato l\'accesso al sito, puoi utilizzare il tuo <span xml:lang="en">account</span> per lasciare delle recensioni riguardo le visite che hai fatto al museo.';
     $operations = '
         <li>
             <a href="ModificaDatiUtente.html" id="ModificaUser" class="button userOperationButton" title="Modifica dati profilo" aria-label="Vai alla pagina per la modifica dei dati del profilo">Modifica profilo</a>
@@ -50,13 +65,14 @@ if ($user['Admin'] === 1) {
         </li>
     ';
 }
-$operations .= '</ul>';
 
 $document = file_get_contents('../HTML/AreaPersonale.html');
 $login = LoginController::getAuthenticationMenu();
 
 $document = str_replace("<span id='loginMenuPlaceholder'/>", $login, $document);
 $document = str_replace("<span id='namePlaceholder'/>", $user['Nome'], $document);
+$document = str_replace("<span id='userDataPlaceholder'/>", $user_data, $document);
+$document = str_replace("<span id='functionalitiesPlaceholder'/>", $functionalities, $document);
 $document = str_replace("<span id='operationsPlaceholder'/>", $operations, $document);
 
 echo $document;
