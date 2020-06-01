862065d8d9407f2fff8639c4989e6e24766b69b1
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-06-02 00:46:29 +0200

    Predisposizione struttura di base

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -15,7 +15,6 @@ function menu() {
     document.getElementById("hamburgerMenu").addEventListener("click", touch);
 }
 
-// Funzione per la rimozione di un messaggio di errore per un determinato input
 function removeError(input) {
     // Recupero del nodo padre dell'input per rimuovergli il nodo figlio (errore)
     const parentNode = input.parentNode;
@@ -27,9 +26,7 @@ function removeError(input) {
     }
 }
 
-// Funzione per l'aggiunta di un messaggio di errore per un determinato input
 function addError(input, error) {
-    // Rimozione di un eventuale errore precedente per evitarene la duplicazione
     removeError(input);
 
     // Recupero del nodo padre dell'input per aggiungergli l'errore come nodo figlio
@@ -39,93 +36,63 @@ function addError(input, error) {
     const span = document.createElement("span");
 
     // Impostazione della classe del nodo
-    span.className("error");
+    span.className = "error";
 
     // Impostazione del testo del nodo
-    // N.B. <var>.innerHTML = ...; non va bene in quanto non viene gestito in modo standard e non gestisce bene l'inserimento di tag HTML annidati
-    span.appendChild(document.createTextElement(error));
+    span.appendChild(document.createTextNode(error));
 
     // Aggiunta del nodo d'errore al nodo padre dell'input
     parentNode.appendChild(span);
 }
 
-// Funzione per il controllo del valore del nome inserito
 function checkName(input) {
-    // Definizione del pattern con cui controllare l'input inserito
     const pattern = new RegExp('^[a-zA-Z]{3,}$');
 
-    // Controllo dell'input tramite il pattern
     if (pattern.test(input.value)) {
-        // Se l'input risulta corretto viene rimosso un eventuale errore precedente e viene ritornato true
         removeError(input);
-
         return true;
     } else {
-        // Se l'input non risulta corretto viene segnalato l'errore e viene ritornato false
         addError(input, "Nome inserito non corretto (almeno 3 caratteri alfabetici)!");
-
         return false;
     }
 }
 
-// Funzione per il controllo del valore del colore inserito
-function checkColor() {
-    // Definizione del pattern con cui controllare l'input inserito
+function checkColor(input) {
     const pattern = new RegExp('^[a-zA-Z]{3,}$');
 
-    // Controllo dell'input tramite il pattern
     if (pattern.test(input.value)) {
-        // Se l'input risulta corretto viene rimosso un eventuale errore precedente e viene ritornato true
         removeError(input);
-
         return true;
     } else {
-        // Se l'input non risulta corretto viene segnalato l'errore e viene ritornato false
         addError(input, "Colore inserito non corretto (almeno 3 caratteri alfabetici)!");
-
         return false;
     }
 }
 
-// Funzione per il controllo del valore del peso inserito
-function checkWeight() {
-    // Definizione del pattern con cui controllare l'input inserito
+function checkWeight(input) {
     const pattern = new RegExp('^[1-9][0-9]{0,2}');
 
-    // Controllo dell'input tramite il pattern
     if (pattern.test(input.value)) {
-        // Se l'input risulta corretto viene rimosso un eventuale errore precedente e viene ritornato true
         removeError(input);
-
         return true;
     } else {
-        // Se l'input non risulta corretto viene segnalato l'errore e viene ritornato false
         addError(input, "Peso inserito non corretto (valore numerico)!");
-
         return false;
     }
 }
 
-// Funzione per il controllo del valore della descrizione inserito
-function checkDescription() {
-    // Definizione del pattern con cui controllare l'input inserito
+function checkDescription(input) {
     const pattern = new RegExp('^[^-\s][a-zA-Z0-9_\s-]+$');
 
-    // Controllo dell'input tramite il pattern
     if (pattern.test(input.value)) {
-        // Se l'input risulta corretto viene rimosso un eventuale errore precedente e viene ritornato true
         removeError(input);
-
         return true;
     } else {
-        // Se l'input non risulta corretto viene segnalato l'errore e viene ritornato false
         addError(input, "Descrizione inserita non corretta!");
-
         return false;
     }
 }
 
-// Funzione per la validazione del form di inserimento di un personaggio
 function formValidation() {
     // Recupero elementi HTML usati per l'inserimento dell'input da parte dell'utente che deve essere controllato
     const name = document.getElementById("nome");
@@ -133,13 +100,11 @@ function formValidation() {
     const weight = document.getElementById("peso");
     const description = document.getElementById("descrizione");
 
-    // N.B. return checkName() && checkColor() && checkWeight() && checkDescription(); Soluzione non ottimale in quanto essendo '&&' un lazy operator e quindi viene visualizzato solo il primo errore!
-
     // Controllo di tutti i valori di input
-    const nameResult = checkName();
-    const colorResult = checkColor();
-    const weightResult = checkWeight();
-    const descriptionResult = checkDescription();
+    const nameResult = checkName(name);
+    const colorResult = checkColor(color);
+    const weightResult = checkWeight(weight);
+    const descriptionResult = checkDescription(description);
 
     // Restituzione del risultato dei controlli
     return nameResult && colorResult && weightResult && descriptionResult;
@@ -162,5 +127,5 @@ function userFormValidation() {
 }
 
 function registrationFormValidation() {
-    
+
 }
