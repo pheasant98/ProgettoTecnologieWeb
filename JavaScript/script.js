bda222e49f431c02b86b56dadd5279c2c3ffcfae
Lorenzo Dei Negri <lorenzodeinegri@gmail.com>
2020-06-02 11:50:12 +0200

    Implementazione controlli recensioni js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,12 +16,9 @@ function menu() {
 }
 
 function removeError(input) {
-    // Recupero del nodo padre dell'input per rimuovergli il nodo figlio (errore)
     const parentNode = input.parentNode;
 
-    // Se il nodo padre dell'input contiene un numero maggiore di 2 nodi figli, ossia <label> e <input>, allora vuol dire che contiene anche il nodo di errore
     if (parentNode.children.length > 2) {
-        // Rimozione dell'ultimo nodo figlio del nodo padre dell'input che risulta essere il nodo di errore in quanto viene sempre aggiunto in coda a quelli presenti
         parentNode.removeChild(parentNode.children[2]);
     }
 }
@@ -29,85 +26,53 @@ function removeError(input) {
 function addError(input, error) {
     removeError(input);
 
-    // Recupero del nodo padre dell'input per aggiungergli l'errore come nodo figlio
     const parentNode = input.parentNode;
-
-    // Creazione del nodo contenente il testo d'errore
     const span = document.createElement("span");
 
-    // Impostazione della classe del nodo
     span.className = "error";
-
-    // Impostazione del testo del nodo
     span.appendChild(document.createTextNode(error));
 
-    // Aggiunta del nodo d'errore al nodo padre dell'input
     parentNode.appendChild(span);
 }
 
-function checkName(input) {
-    const pattern = new RegExp('^[a-zA-Z]{3,}$');
+function checkReviewTitle(input) {
+    const title = input.value;
+    const pattern = new RegExp('^[A-zÀ-ú0-9\'`!?.,:\s()\-]+$');
 
-    if (pattern.test(input.value)) {
+    if (title.length === 0) {
+        addError(input, 'Non è possibile inserire una recensione con un titolo vuoto');
+        return false;
+    } else if (title.length < 2) {
+        addError(input, 'Non è possibile inserire una recensione con un titolo più corto di 2 caratteri');
+        return false;
+    } else if (title.length > 64) {
+        addError(input, 'Non è possibile inserire una recensione con un titolo più lungo di 64 caratteri');
+        return false;
+    } else if (!pattern.test(title)) {
+        addError(input, 'Il titolo inserito contiene dei caratteri non consentiti, è possibile inserire solamente lettere, possibilmente accentate, numeri, parentesi e segni di punteggiatura');
+        return false;
+    } else {
         removeError(input);
         return true;
-    } else {
-        addError(input, "Nome inserito non corretto (almeno 3 caratteri alfabetici)!");
-        return false;
     }
 }
 
-function checkColor(input) {
-    const pattern = new RegExp('^[a-zA-Z]{3,}$');
+function checkReviewContent(input) {
+    const content = input.value;
 
-    if (pattern.test(input.value)) {
+    if (content.length === 0) {
+        addError(input, 'Non è possibile inserire una recensione con un contenuto vuoto');
+        return false;
+    } else if (content.length  < 4) {
+        addError(input, 'Non è possibile inserire una recensione con un contenuto più corto di 4 caratteri');
+        return false;
+    } else if (content.length  > 500) {
+        addError(input, 'Non è possibile inserire una recensione con un contenuto più lungo di 500 caratteri');
+        return false;
+    } else {
         removeError(input);
         return true;
-    } else {
-        addError(input, "Colore inserito non corretto (almeno 3 caratteri alfabetici)!");
-        return false;
     }
-}
-
-function checkWeight(input) {
-    const pattern = new RegExp('^[1-9][0-9]{0,2}');
-
-    if (pattern.test(input.value)) {
-        removeError(input);
-        return true;
-    } else {
-        addError(input, "Peso inserito non corretto (valore numerico)!");
-        return false;
-    }
-}
-
-function checkDescription(input) {
-    const pattern = new RegExp('^[^-\s][a-zA-Z0-9_\s-]+$');
-
-    if (pattern.test(input.value)) {
-        removeError(input);
-        return true;
-    } else {
-        addError(input, "Descrizione inserita non corretta!");
-        return false;
-    }
-}
-
-function formValidation() {
-    // Recupero elementi HTML usati per l'inserimento dell'input da parte dell'utente che deve essere controllato
-    const name = document.getElementById("nome");
-    const color = document.getElementById("colore");
-    const weight = document.getElementById("peso");
-    const description = document.getElementById("descrizione");
-
-    // Controllo di tutti i valori di input
-    const nameResult = checkName(name);
-    const colorResult = checkColor(color);
-    const weightResult = checkWeight(weight);
-    const descriptionResult = checkDescription(description);
-
-    // Restituzione del risultato dei controlli
-    return nameResult && colorResult && weightResult && descriptionResult;
 }
 
 function artworkFormValidation() {
@@ -119,7 +84,13 @@ function eventFormValidation() {
 }
 
 function reviewFormValidation() {
+    const title = document.getElementById("title");
+    const content = document.getElementById("content");
 
+    const titleResult = checkReviewTitle(title);
+    const contentResult = checkReviewContent(content);
+
+    return titleResult && contentResult;
 }
 
 function userFormValidation() {
